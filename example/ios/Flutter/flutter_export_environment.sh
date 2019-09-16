27acb790abdb76ee89a67e448840c8fdbf4dc9de
柯军 <kej@meiyibao.com>
2019-09-16 15:34:16 +0800

    二维码扫描插件用本地库

---
+++ example/ios/Flutter/flutter_export_environment.sh
@@ -0,0 +1,8 @@
+#!/bin/sh
+# This is a generated file; do not edit or check into version control.
+export "FLUTTER_ROOT=/home/jameskarl/devenv/dart/flutter-stable"
+export "FLUTTER_APPLICATION_PATH=/home/jameskarl/myb/code/flutter/ShopMobile/github/flutter_barcode_reader/example"
+export "FLUTTER_TARGET=lib/main.dart"
+export "FLUTTER_BUILD_DIR=build"
+export "SYMROOT=${SOURCE_ROOT}/../build/ios"
+export "FLUTTER_FRAMEWORK_DIR=/home/jameskarl/devenv/dart/flutter-stable/bin/cache/artifacts/engine/ios"
